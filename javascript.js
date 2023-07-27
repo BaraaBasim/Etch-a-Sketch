f57bfce70b0956cf20000a3389c1527b7c388dbd
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2023-07-28 01:44:32 +0800

    title

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("gridContainer");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 200; i++) {
   const div = document.createElement("div");
   div.classList.add("gridBox");
   container.appendChild(div);
